b1035491859b5a6ded6e07b821c8abd26cf2aa58
FreakyCoder <kurayogun@gmail.com>
2023-04-10 13:38:55 +0300

    feat: version 2 with modern functional component and more features

--- example/App.tsx
+++ example/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {SafeAreaView, View, Image, Text, StatusBar} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import CounterInput from 'react-native-counter-input';
-
+// import CounterInput from 'react-native-counter-input';
+import CounterInput from './lib/CounterInput';
 const App = () => {
   return (
     <>
